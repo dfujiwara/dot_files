7abfae86e6fa678e85e29f78e5b3711b1d89b3fc
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2019-10-21 21:57:12 -0700

    Install vscode related settings

--- setup.sh
+++ setup.sh
@@ -12,6 +12,15 @@ ln -sf $script_dir/bash_aliases ~/.bash_aliases
 # Xcode keybindings set up
 ln -sf $script_dir/iOS/Daisuke.idekeybindings  ~/Library/Developer/Xcode/UserData/KeyBindings/Daisuke.idekeybindings 
 
+# VSCode related set up
+for extension in $(cat $script_dir/vscode/extensions.txt)
+do
+    code --install-extension $extension
+done
+
+ln -s -f $script_dir/vscode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json   
+ln -s -f $script_dir/vscode/settings.json ~/Library/Application\ Support/Code/User/settings.json   
+
 # Vim plugin set up
 vim_pack_dir=~/.vim/pack/plugins
 mkdir -p $vim_pack_dir
@@ -19,3 +28,4 @@ git clone https://github.com/itchyny/lightline.vim $vim_pack_dir/start/lightline
 git clone https://github.com/scrooloose/nerdtree.git $vim_pack_dir/start/nerdtree
 git clone https://github.com/ctrlpvim/ctrlp.vim $vim_pack_dir/start/ctrlp
 
+echo 'Done'
