6dd3bedebd7ccef2b4bf71d941a92668278a23c6
Daisuke Fujiwara <daisuke.fujiwara@gmail.com>
2020-04-01 21:27:15 -0700

    Install zsh plugins via git clone

--- setup.sh
+++ setup.sh
@@ -33,7 +33,9 @@ brew cask install rectangle
 brew cask install flux
 brew cask install docker
 
-# zsh 
+# zsh + plugins
 ln -sf $script_dir/zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 echo 'Done'
